e2e97076be711c6f4326ef1b50eb436702bffda0
prokofiew <filipmaciborski@gmail.com>
2023-11-10 09:42:39 +0100

    Adding TextStyle() like fontSize, color and custom font

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('Hi Filip!'),
+      title: const Text(
+        'Hi Filip!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+        ),
+      ),
       centerTitle: true,
+      backgroundColor: Colors.red[600],
     ),
     body: Center(
-      child: Text('App body is here'),
+      child: Text(
+          'App body is here',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight:  FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.grey[600],
+          fontFamily: "IndieFlower",
+        )
+      ),
+    ),
+    bottomNavigationBar: BottomAppBar(
+    shape: const CircularNotchedRectangle(),
+    child: Container(height: 50.0)
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {  },
-      child: Text('Click')
+      backgroundColor: Colors.red[600],
+      child: const Text('Click'),
     ),
   ),
 ));
